2fa89fe4f044f47432be4c2eda1b335c0a03ac8a
Yaroslav Tsebro <yaroslav.cebro@gmail.com>
2023-08-21 16:05:09 +0300

    added root

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.scss";
+import { getProductsByLimit } from "./http/product-api";
+import { Product } from "./types/product";
+import { ProductCard } from "./components/ProductCard/ProductCard";
+import { Shop } from "./pages/Shop/Shop";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Shop />;
 }
 
 export default App;
